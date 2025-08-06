5f34b218baae06f912861efda3127345488aaef7
Ozan-Alcanoglu <ozanalcanoglu36@gmail.com>
2025-08-06 22:02:56 +0300

    added eureka

--- product-service/src/main/java/feign_demo/feign/UserClient.java
+++ product-service/src/main/java/feign_demo/feign/UserClient.java
@@ -3,7 +3,7 @@ package feign_demo.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "user-service", url = "http://localhost:8081")
+@FeignClient(name = "user-service")
 public interface UserClient {
 
     @GetMapping("/users")
